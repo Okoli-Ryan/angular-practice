661e794e74b082865f51fbd9b3ce1a286ae083fa
Ugo Okoli <ookoli@systemspecs.com.ng>
2023-08-11 22:27:47 +0100

    interactive-rating

--- src/app/pages/interactive-rating/interactive-rating.component.ts
+++ src/app/pages/interactive-rating/interactive-rating.component.ts
@@ -1,10 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CenterLayoutComponent } from 'src/app/layouts/center-layout/center-layout.component';
 
 @Component({
+  standalone: true,
   selector: 'app-interactive-rating',
   templateUrl: './interactive-rating.component.html',
-  styleUrls: ['./interactive-rating.component.scss']
+  styleUrls: ['./interactive-rating.component.scss'],
+  imports: [CenterLayoutComponent, CommonModule]
 })
 export class InteractiveRatingComponent {
 
+  selectedRating: number = -1;
+  isSubmitted: boolean = false;
+
+  selectRating(rating: number) {
+    this.selectedRating = rating
+  }
+
+  submit() {
+    this.isSubmitted = true
+  }
 }
