4301bb5a756e06e82327d0590bca17d13f13aeba
Ugo Okoli <ookoli@systemspecs.com.ng>
2023-08-11 11:56:24 +0100

    frontend-mentor-2

---
+++ src/app/pages/frontend-mentor-two/frontend-mentor-two.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CenterLayoutComponent } from 'src/app/layouts/center-layout/center-layout.component';
+
+@Component({
+  standalone: true,
+  selector: 'app-frontend-mentor-two',
+  templateUrl: './frontend-mentor-two.component.html',
+  styleUrls: ['./frontend-mentor-two.component.scss'],
+  imports: [CenterLayoutComponent]
+})
+export class FrontendMentorTwoComponent {
+
+}
