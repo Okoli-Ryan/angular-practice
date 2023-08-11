6b2927e8123a9eee135279b11870d7ed4b628d5a
Ugo Okoli <ookoli@systemspecs.com.ng>
2023-08-11 13:31:29 +0100

    frontend-mentor-3

---
+++ src/app/pages/frontend-mentor-four/frontend-mentor-four.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CenterLayoutComponent } from 'src/app/layouts/center-layout/center-layout.component';
+
+@Component({
+  standalone: true,
+  selector: 'app-frontend-mentor-four',
+  templateUrl: './frontend-mentor-four.component.html',
+  styleUrls: ['./frontend-mentor-four.component.scss'],
+  imports: [CenterLayoutComponent]
+})
+export class FrontendMentorFourComponent {
+
+}
---
+++ src/app/pages/frontend-mentor-three/frontend-mentor-three.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CenterLayoutComponent } from 'src/app/layouts/center-layout/center-layout.component';
+
+@Component({
+  standalone: true,
+  selector: 'app-frontend-mentor-three',
+  templateUrl: './frontend-mentor-three.component.html',
+  styleUrls: ['./frontend-mentor-three.component.scss'],
+  imports: [CenterLayoutComponent]
+})
+export class FrontendMentorThreeComponent {
+
+}
